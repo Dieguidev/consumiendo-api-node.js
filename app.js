ee150475e8fd22f120d0d2843171c27f866b208e
Dieguidev <diegogaraycullas@gmail.com>
2023-06-19 22:02:43 -0500

    status 500

--- app.js
+++ app.js
@@ -13,53 +13,45 @@
 //   })
 
 const axios = require('axios');
-const querystring = require('querystring');
 
 const clientId = 'pikango';
 const clientSecret = '6UGoAJtXa4Kwo0Sa6ZAZhx0zZy0JBcnLEs04Fjsb2DBJfTn7uUNYdH9JSaVaA1j';
-const redirectUri = 'https://cloud.securitasfleet.com//api-fleet/v2/trackables';
+const apiUrl = 'https://cloud.securitasfleet.com/api-fleet/v2/trackables';
 
-// Paso 1: Obtener el código de autorización
-const getAuthorizationCode = async () => {
-  const params = {
-    client_id: clientId,
-    redirect_uri: redirectUri,
-    response_type: 'code'
-  };
-
-  const authorizationUrl = `https://proveedor-oauth.com/auth?${querystring.stringify(params)}`;
-  console.log('Abre esta URL en tu navegador:', authorizationUrl);
-
-  // Espera a que el usuario ingrese el código de autorización
-  const authorizationCode = 'CODIGO_DE_AUTORIZACION';
-
-  return authorizationCode;
-};
-
-// Paso 2: Obtener el token de acceso
-const getAccessToken = async (authorizationCode) => {
+// Función para obtener el token de acceso
+const getAccessToken = async () => {
+  const tokenUrl = 'https://cloud.securitasfleet.com/token';
   const params = {
     client_id: clientId,
     client_secret: clientSecret,
-    redirect_uri: redirectUri,
-    code: authorizationCode,
-    grant_type: 'authorization_code',
+    grant_type: 'client_credentials',
   };
 
-  const tokenUrl = 'https://proveedor-oauth.com/token';
-
   try {
-    const response = await axios.post(tokenUrl, querystring.stringify(params));
-    const { access_token, refresh_token } = response.data;
-    console.log('Access Token:', access_token);
-    console.log('Refresh Token:', refresh_token);
+    const response = await axios.post(tokenUrl, params);
+    const { access_token } = response.data;
+    return access_token;
   } catch (error) {
     console.error('Error al obtener el token de acceso:', error.message);
+    throw error;
   }
 };
 
-// Ejecutar el flujo de autorización
-(async () => {
-  const authorizationCode = await getAuthorizationCode();
-  await getAccessToken(authorizationCode);
-})();
+// Función para realizar la solicitud a la API
+const makeApiRequest = async () => {
+  try {
+    const accessToken = await getAccessToken();
+
+    const headers = {
+      Authorization: `Bearer ${accessToken}`,
+    };
+
+    const response = await axios.get(apiUrl, { headers });
+    console.log('Respuesta de la API:', response.data);
+  } catch (error) {
+    console.error('Error al realizar la solicitud a la API:', error.message);
+  }
+};
+
+// Ejecutar la solicitud a la API
+makeApiRequest();
